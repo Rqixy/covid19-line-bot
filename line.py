18cafae88a10023b714f45ce504ef6ab03142ffb
Rqixy <integral.ys1013@gmail.com>
2022-02-17 20:39:00 +0900

    fixed line.py

--- line.py
+++ line.py
@@ -14,8 +14,8 @@ import os
 app = Flask(__name__)
 
 # 環境変数取得
-LINE_CHANNEL_ACCESS_TOKEN = os.environ("LINE_CHANNEL_ACCESS_TOKEN")
-LINE_CHANNEL_SECRET = os.environ("LINE_CHANNEL_SECRET")
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
