179ab140c328a69c8fd540079d9c8337d385888d
Rqixy <integral.ys1013@gmail.com>
2022-02-17 20:34:29 +0900

    fixed line.py

--- line.py
+++ line.py
@@ -47,10 +47,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
-        event.reply_message,
-        TextSendMessage(text=event.message.text)
-    )
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 # ポートの設定
-port = int(os.getenv("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
