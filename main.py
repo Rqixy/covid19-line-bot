a0d24f1e0df4015a9249a64d457010d62088a0a2
Rqixy <integral.ys1013@gmail.com>
2022-02-18 18:53:57 +0900

    add buttons

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from flask import Flask, request, abort
 
 from linebot import (
@@ -8,17 +9,32 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CameraAction, CameraRollAction, LocationAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    MemberJoinedEvent, MemberLeftEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, IconComponent, ButtonComponent,
+    SeparatorComponent, QuickReply, QuickReplyButton,
+    ImageSendMessage
 )
 import os
 import db
 
 # データベースから最新のデータ情報を持ってくる
 new_data_array = db.print_new_data()
-line_text_new_data = new_data_array[0] + "\n" + new_data_array[1] + "\n" + new_data_array[2] + "\n" + new_data_array[3] + "\n" "詳しくは下記のURLから確認してね！" + "\n\n" + "https://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html"
+line_text_new_data = new_data_array[0] + "\n" + new_data_array[1] + "\n" + new_data_array[2] + "\n" + new_data_array[3] + "\n詳しくは下記のURLから確認してね！" + "\n\n" + "https://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html"
 # print(line_text_new_data)
 
 week_data_array = db.print_week_data()
-line_text_week_data = week_data_array[0] + "\n" + week_data_array[1] + "\n" + week_data_array[2] + "\n" + week_data_array[3] + "\n" + week_data_array[4] + "\n" + week_data_array[5] + "\n" + week_data_array[6] + "\n\n" + "https://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html"
+line_text_week_data = week_data_array[0] + "\n" + week_data_array[1] + "\n" + week_data_array[2] + "\n" + week_data_array[3] + "\n" + week_data_array[4] + "\n" + week_data_array[5] + "\n" + week_data_array[6] + "\n詳しくは下記のURLから確認してね！" + "\n\n" + "https://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html"
+
 
 app = Flask(__name__)
 
@@ -50,11 +66,26 @@ def callback():
     # handleの処理を終えればOK
     return 'OK'
 
+@handler.add(FollowEvent)
+def handle_follow(event):
+    if event.type == 'follow':
+        confirm_buttons = ConfirmTemplate(
+            text="友達追加ありがとうございいます！\n\n毎朝7時に最新のコロナ感染人数を送信するよ！\n\n最新のコロナ感染情報を知りたい場合は、\"最新\"\n1週間のコロナ感染情報を知りたい場合は、\"1週間\"を入力してね！\n\nまた下のボタンからでも確認できるよ！\n\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n詳しくはこちらのサイトから確認してね！",
+            actions=[
+                MessageAction(label="最新", text=line_text_new_data),
+                MessageAction(label="1週間", text=line_text_week_data)
+            ]
+        )
+        line_bot_api.reply_message(
+            event.reply_token,
+            confirm_buttons
+        )
 
 # Lineのメッセージの取得と返信内容の設定
 # LINEでMessageEventが起こった場合に、def以下の関数を実行する
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
     text = event.message.text
     if text in ['最新', '最新情報']:
         line_bot_api.reply_message(
