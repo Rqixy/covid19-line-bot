cd06008286d6f8ba211f868aa08b4b9a94e86e5f
Rqixy <integral.ys1013@gmail.com>
2022-02-20 15:49:22 +0900

    感染者数の表示文字修正

--- db.py
+++ db.py
@@ -78,7 +78,7 @@ def print_new_infected_data():
             curs.execute(sql)
             new_data = []
             for row in curs.fetchall():
-                new_data = [row[4], "新規感染者：" + str(row[1]) + "人", "重症者：" + str(row[2]) + "人", "死亡者：" + str(row[3]) + "人"]
+                new_data = [row[4], "新規感染者数：" + str(row[1]) + "人", "重症者数(累計)：" + str(row[2]) + "人", "死亡者数(累計)：" + str(row[3]) + "人"]
             return new_data
 
 # 1週間分の感染情報を取得して返す
@@ -90,7 +90,7 @@ def print_week_infected_data():
             curs.execute(sql)
             week_data = []
             for row in curs.fetchall():
-                week_data.append(row[4] + "\n" + "    新規感染者：" + str(row[1]) + "人" + "\n" + "    重症者：" + str(row[2]) + "人" + "\n" + "    死亡者：" + str(row[3]) + "人")
+                week_data.append(row[4] + "\n" + "    新規感染者数：" + str(row[1]) + "人" + "\n" + "    重症者数(累計)：" + str(row[2]) + "人" + "\n" + "    死亡者数(累計)：" + str(row[3]) + "人")
             return week_data
 
 # user_idを配列で取得して返す
