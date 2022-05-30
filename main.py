8f2533621e93a81858aca40f26f9ed15763d090b
Rqixy <integral.ys1013@gmail.com>
2022-05-30 15:15:12 +0900

    3日前まで個々に表示できるようにした

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ def make_quick_reply(token, text):
     items = []
     items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
     items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
+    items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
+    items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
     items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
     messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
     line_bot_api.reply_message(token, messages=messages)
