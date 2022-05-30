497481938114945cb7fe1b9a4f2be86220cbff3a
Rqixy <integral.ys1013@gmail.com>
2022-05-30 14:20:02 +0900

    エラー時の処理追加

--- scraping.py
+++ scraping.py
@@ -11,9 +11,6 @@ options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 10)
 
-class NullTextException(Exception):
-    pass
-
 # スクレイピング部分
 def covid19_scraping(iframe_xpath: string, scraping_xpath: string) -> string:
     # 取得出来るまで繰り返す(無限ループ阻止のため最大20回まで)
@@ -22,7 +19,7 @@ def covid19_scraping(iframe_xpath: string, scraping_xpath: string) -> string:
         # iframeに入る
         iframe = driver.find_element(by=By.XPATH, value=iframe_xpath)
         driver.switch_to.frame(iframe)
-        time.sleep(1)
+        # time.sleep(1)
         # スクレイピングする
         result = driver.find_element(by=By.XPATH, value=scraping_xpath)
         print('result.text : ' + result.text)
@@ -43,53 +40,58 @@ def remove_comma_and_text_to_int(text: string) -> int:
     return text
 
 def infected_people_scraping():
-    driver.get('https://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html')
-    # ページが読み込まれるまで待機
-    wait.until(EC.presence_of_all_elements_located)
+    try:
+        driver.get('https://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html')
+        # ページが読み込まれるまで待機
+        wait.until(EC.presence_of_all_elements_located)
 
-    # 更新チェック用の日付を取得する
-    infected_day_iframe_xpath = '/html/body/div[1]/main/div[2]/div/div/div[3]/div/iframe'
-    infected_day_xpath = '/html/body/main/div/div/div/div/div/h3/span'
-    infected_day = covid19_scraping(infected_day_iframe_xpath, infected_day_xpath)
-    infected_day = infected_day + "0:00現在"
-    # 元のフレームに戻る
-    driver.switch_to.default_content()
+        # 更新チェック用の日付を取得する
+        infected_day_iframe_xpath = '/html/body/div[1]/main/div[2]/div/div/div[3]/div/iframe'
+        infected_day_xpath = '/html/body/main/div/div/div/div/div/h3/span'
+        infected_day = covid19_scraping(infected_day_iframe_xpath, infected_day_xpath)
+        infected_day = infected_day + "0:00現在"
+        # 元のフレームに戻る
+        driver.switch_to.default_content()
 
-    # 新規感染者数
-    new_iframe_xpath = '/html/body/div[1]/main/div[2]/div/div/div[4]/div[1]/iframe'   # 新規感染者のiframe
-    new_xpath = '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
-    new_people = covid19_scraping(new_iframe_xpath, new_xpath)
-    # 数値型に変換する
-    new_people = remove_comma_and_text_to_int(new_people)
-    # 元のフレームに戻る
-    driver.switch_to.default_content()
+        # 新規感染者数
+        new_iframe_xpath = '/html/body/div[1]/main/div[2]/div/div/div[4]/div[1]/iframe'   # 新規感染者のiframe
+        new_xpath = '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
+        new_people = covid19_scraping(new_iframe_xpath, new_xpath)
+        # 数値型に変換する
+        new_people = remove_comma_and_text_to_int(new_people)
+        # 元のフレームに戻る
+        driver.switch_to.default_content()
 
-    # 重症者数
-    severe_iframe_xpath = '/html/body/div[1]/main/div[2]/div/div/div[4]/div[4]/iframe'   # 重症者のiframe
-    severe_xpath = '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
-    severe_people = covid19_scraping(severe_iframe_xpath, severe_xpath)
-    # 数値型に変換する
-    severe_people = remove_comma_and_text_to_int(severe_people)
-    # 元のフレームに戻る
-    driver.switch_to.default_content()
+        # 重症者数
+        severe_iframe_xpath = '/html/body/div[1]/main/div[2]/div/div/div[4]/div[4]/iframe'   # 重症者のiframe
+        severe_xpath = '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
+        severe_people = covid19_scraping(severe_iframe_xpath, severe_xpath)
+        # 数値型に変換する
+        severe_people = remove_comma_and_text_to_int(severe_people)
+        # 元のフレームに戻る
+        driver.switch_to.default_content()
 
-    # 死亡者数
-    deaths_iframe_xpath = '/html/body/div[1]/main/div[2]/div/div/div[4]/div[3]/iframe'   # 死亡者のiframe
-    deaths_xpath = '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
-    deaths = covid19_scraping(deaths_iframe_xpath, deaths_xpath)
-    # 数値型に変換する
-    deaths = remove_comma_and_text_to_int(deaths)
-    # 元のフレームに戻る
-    driver.switch_to.default_content()
+        # 死亡者数
+        deaths_iframe_xpath = '/html/body/div[1]/main/div[2]/div/div/div[4]/div[3]/iframe'   # 死亡者のiframe
+        deaths_xpath = '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
+        deaths = covid19_scraping(deaths_iframe_xpath, deaths_xpath)
+        # 数値型に変換する
+        deaths = remove_comma_and_text_to_int(deaths)
+        # 元のフレームに戻る
+        driver.switch_to.default_content()
 
-    # ドライバーを閉じる
-    driver.quit()
+        # ドライバーを閉じる
+        driver.quit()
 
-    # 1つでも中身が空だったらNoneを送る
-    if new_people == None or severe_people == None or deaths == None or infected_day == None:
+        # 1つでも中身が空だったらNoneを送る
+        if new_people == None or severe_people == None or deaths == None or infected_day == None:
+            return None
+
+        # 配列にスクレイピングしたデータを格納する
+        infected_people = [new_people, severe_people, deaths, infected_day]
+        print(infected_people)
+        return infected_people
+    except Exception as e:
+        # 情報の取得に失敗したら、コンソールにエラーメッセージ表示とNoneを返す
+        print("Error : " + e)
         return None
-
-    # 配列にスクレイピングしたデータを格納する
-    infected_people = [new_people, severe_people, deaths, infected_day]
-    print(infected_people)
-    return infected_people
