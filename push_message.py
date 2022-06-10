9f72f13d760641683bbd8cc5ad09f5a2c809c8e4
Rqixy <integral.ys1013@gmail.com>
2022-06-10 14:11:53 +0900

    クイックメッセージ処理をモジュール化のため、make_quick_reply削除

--- push_message.py
+++ push_message.py
@@ -1,11 +1,9 @@
 # 午後1時に自動送信するプログラム
-
 from linebot import LineBotApi
-from linebot.models import TextSendMessage, QuickReply, QuickReplyButton
-from linebot.models.actions import PostbackAction
 import os
 import db
-
+import sys
+import message
 
 # データベースから最新のデータ情報を持ってくる
 new_data = db.insert_infected_data()
@@ -16,21 +14,13 @@ if type(new_data) is str:
     line_text_new_data = new_data + "\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
     print(line_text_new_data)
 # もし新しいデータが更新されていなかったらdbのinsert_infected_peopleの配列結果を代入する
-else:
+elif type(new_data) is list:
     # Lineに送る送信メッセージの作成
     line_text_new_data = new_data[0] + "\n" + new_data[1] + "\n" + new_data[2] + "\n" + new_data[3] + "\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
     print(line_text_new_data)
-
-# クイックリプライ
-def make_quick_reply(user_id, text):
-    items = []
-    items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
-    items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
-    items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
-    items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
-    items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
-    messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
-    line_bot_api.push_message(user_id, messages=messages)
+else:
+    print("ERROR : 情報が何か変です！" + new_data)
+    sys.exit()
 
 # アクセストークンの取得
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
@@ -41,4 +31,4 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 # 登録されているuser_idの人達に最新情報を送信する
 for user_id in users_id:
     USER_ID = user_id
-    make_quick_reply(USER_ID, text=line_text_new_data)
+    message.make_quick_message(user_id=USER_ID, text=line_text_new_data, line_bot_api=line_bot_api)
