2a2312f1a1c3ddabfa5df98a1b3bc3b5444860f9
Rqixy <integral.ys1013@gmail.com>
2022-06-10 15:39:40 +0900

    main用のクイックメッセージ削除

--- message.py
+++ message.py
@@ -3,7 +3,7 @@ from linebot.models import TextSendMessage, QuickReply, QuickReplyButton
 from linebot.models.actions import PostbackAction
 
 # プッシュ用のクイックメッセージの生成メソッド
-def make_quick_message(text, line_bot_api, token=None, user_id=None):
+def make_quick_message(text, line_bot_api, user_id):
     try:
         items = []
         items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
@@ -12,25 +12,7 @@ def make_quick_message(text, line_bot_api, token=None, user_id=None):
         items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
         items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
         messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
-        # 個人からのメッセージが送られたら
-        if token != None:
-            return line_bot_api.push_message(token, messages=messages)
-        # 自動送信でuser_idを受けっとたら
-        elif user_id != None:
-            return line_bot_api.push_message(user_id, messages=messages)
-    except Exception as e:
-        print("メッセージエラー発生！ : " + str(type(e)) + " : " + e.args + " : " + e.message)
-
-def make_main_quick_message(text, line_bot_api, token):
-    try:
-        items = []
-        items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
-        items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
-        items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
-        items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
-        items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
-        messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
-        # 個人からのメッセージが送られたら
-        return line_bot_api.push_message(token, messages=messages)
+        
+        return line_bot_api.push_message(user_id, messages=messages)
     except Exception as e:
         print("メッセージエラー発生！ : " + str(type(e)) + " : " + e.args + " : " + e.message)
