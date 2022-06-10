13c64c7d0e445e8d861aea018381732d227661f7
Rqixy <integral.ys1013@gmail.com>
2022-06-10 14:20:00 +0900

    クイックメッセージのモジュール作成

---
+++ message.py
@@ -0,0 +1,19 @@
+# クイックメッセージを利用するためのモジュール
+from linebot.models import TextSendMessage, QuickReply, QuickReplyButton
+from linebot.models.actions import PostbackAction
+
+# プッシュ用のクイックメッセージの生成メソッド
+def make_quick_message(text: str, line_bot_api: any, token='', user_id=''):
+    items = []
+    items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
+    items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
+    items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
+    items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
+    items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
+    messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
+    # 個人からのメッセージが送られたら
+    if token != '' and user_id == '':
+        line_bot_api.push_message(token, messages=messages)
+    # 自動送信でuser_idを受けっとたら
+    elif user_id != '' and token == '':
+        line_bot_api.push_message(user_id, messages=messages)
