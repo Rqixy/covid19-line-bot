e09f6416e37c80b3198786e058167310f390cb79
Rqixy <integral.ys1013@gmail.com>
2022-06-10 15:14:12 +0900

    tokenを文字列型にキャスト

--- message.py
+++ message.py
@@ -14,7 +14,7 @@ def make_quick_message(text, line_bot_api, token=None, user_id=None):
         messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
         # 個人からのメッセージが送られたら
         if token != None and user_id == None:
-            return line_bot_api.push_message(token, messages=messages)
+            return line_bot_api.push_message(str(token), messages=messages)
         # 自動送信でuser_idを受けっとたら
         elif token == None and user_id != None:
             return line_bot_api.push_message(user_id, messages=messages)
