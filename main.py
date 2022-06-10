e59d0eae002d17a1528e2a721d6a824253130a10
Rqixy <integral.ys1013@gmail.com>
2022-06-10 15:39:14 +0900

    クイックメッセージ追加

--- main.py
+++ main.py
@@ -12,30 +12,27 @@ from linebot.models import (
 from linebot.models.actions import PostbackAction
 import os
 import db
-import message
 
 # クイックリプライの処理
-# def make_quick_reply(token, text):
-#     items = []
-#     items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
-#     items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
-#     items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
-#     items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
-#     items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
-#     messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
-#     line_bot_api.reply_message(token, messages=messages)
+def make_quick_reply(token, text):
+    items = []
+    items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
+    items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
+    items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
+    items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
+    items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
+    messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
+    line_bot_api.reply_message(token, messages=messages)
 
 # 1日分のコロナ感染者情報の送信の処理
-def infected_data_reply(token: any, day: int, line_bot_api: any):
+def infected_data_reply(event: any, day: int):
     new_data_array = db.print_infected_data(day=day)
     # もし1週間の範囲外の数値が与えられたら範囲外のメッセージを送信する
     if type(new_data_array) is str:
-        # make_quick_reply(event.reply_token, text=new_data_array)
-        message.make_main_quick_message(token=token, text=new_data_array, line_bot_api=line_bot_api)
+        make_quick_reply(event.reply_token, text=new_data_array)
         return
     line_text_new_data = new_data_array[0] + "\n" + new_data_array[1] + "\n" + new_data_array[2] + "\n" + new_data_array[3] + "\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
-    # make_quick_reply(event.reply_token, text=line_text_new_data)
-    message.make_main_quick_message(token=token, text=line_text_new_data, line_bot_api=line_bot_api)
+    make_quick_reply(event.reply_token, text=line_text_new_data)
 
 app = Flask(__name__)
 
@@ -75,9 +72,7 @@ def handle_follow(event):
     # データベースにuser_idを格納する
     db.insert_user_data(user_id)
     # quick replyを表示する
-    token = event.reply_token
-    text = "友だち追加ありがとうございます\n\n午後1時に最新のコロナ感染人数を送信するよ！\n\n最新のコロナ感染情報を知りたい場合は、\"最新\"\n1週間のコロナ感染情報を知りたい場合は、\"1周間\"\nと入力してください！\n\nまた下のメッセージボタンからでも確認できるよ！\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
-    message.make_main_quick_message(token=token, text=text, line_bot_api=line_bot_api)
+    make_quick_reply(event.reply_token, text="友だち追加ありがとうございます\n\n午後1時に最新のコロナ感染人数を送信するよ！\n\n最新のコロナ感染情報を知りたい場合は、\"最新\"\n1週間のコロナ感染情報を知りたい場合は、\"1周間\"\nと入力してください！\n\nまた下のメッセージボタンからでも確認できるよ！\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n")
 
 # Lineのメッセージの取得と返信内容の設定
 @handler.add(MessageEvent, message=TextMessage)
@@ -86,30 +81,23 @@ def handle_message(event):
     # 最新の情報を送信する
     if text == '最新' or text == '最新情報':
         day = 0
-        token = event.reply_token
-        infected_data_reply(token=token, day=day, line_bot_api=line_bot_api)
+        infected_data_reply(event=event, day=day)
     # 一日前の情報を送信する
     elif text == '昨日' or text == '1日前' or text == '一日前' or text == 'yesterday':
         day = 1
-        token = event.reply_token
-        infected_data_reply(token=token, day=day, line_bot_api=line_bot_api)
+        infected_data_reply(event=event, day=day)
     elif text == '一昨日' or text == '2日前' or text == '二日前':
         day = 2
-        token = event.reply_token
-        infected_data_reply(token=token, day=day, line_bot_api=line_bot_api)
+        infected_data_reply(event=event, day=day)
     elif text == '3日前' or text == '三日前':
         day = 3
-        token = event.reply_token
-        infected_data_reply(token=token, day=day, line_bot_api=line_bot_api)
+        infected_data_reply(event=event, day=day)
     elif text == '1週間' or text == '１週間' or text == '一週間' or text == 'week':
-        token = event.reply_token
         week_data_array = db.print_week_infected_data()
         line_text_week_data = week_data_array[0] + week_data_array[1] + week_data_array[2] + week_data_array[3] + week_data_array[4] + week_data_array[5] + week_data_array[6] + "\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
-        message.make_main_quick_message(token=token, text=line_text_week_data, line_bot_api=line_bot_api)
+        make_quick_reply(event.reply_token, text=line_text_week_data)
     else:
-        token = event.reply_token
-        text = "入力する言葉が違うよ！\n\n最新情報は\"最新\"\n一週間の情報は\"一週間\"\n\nと入力してね！\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
-        message.make_main_quick_message(token=token, text=text, line_bot_api=line_bot_api)
+        make_quick_reply(event.reply_token, text="入力する言葉が違うよ！\n\n最新情報は\"最新\"\n一週間の情報は\"一週間\"\n\nと入力してね！\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n")
 
 # ブロックしたらデータベースからuser_idを削除する
 @handler.add(UnfollowEvent)
