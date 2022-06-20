3786c3a1d680d5845f201689050520208db7f4c2
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-06-20 10:51:55 +0900

    スクレピングフォルダーに統一

---
+++ scraping/scraping_config.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# WebDriverの設定
+def setting_web_driver() -> webdriver:
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+# 待機時間
+def waiting_open_website():
+    driver = setting_web_driver()
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_all_elements_located)
+
+# 文字列に含まれるカンマを取り除く
+def remove_comma(text: str) -> str:
+    text = text.replace(',','')
+    return text
+
+# 文字列型の数字列を整数型に変換する
+def str_to_int(text: str) -> int:
+    text = int(text)
+    return text
+
+# 配列がちゃんと取得できているかチェック
+def check_array(array):
+    for check in array:
+        if check == None:
+            return None
+    # うまく取得できたら配列を返す
+    return array
