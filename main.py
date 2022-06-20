56c21fb160a7ea342042a51da2a0505bc6a31891
Rqixy <integral.ys1013@gmail.com>
2022-06-20 22:25:37 +0900

    スクレピング検証用コメント変更

--- main.py
+++ main.py
@@ -99,8 +99,8 @@ def handle_message(event):
         line_text_week_data = week_data_array[0] + week_data_array[1] + week_data_array[2] + week_data_array[3] + week_data_array[4] + week_data_array[5] + week_data_array[6] + "\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
         make_quick_reply(event.reply_token, text=line_text_week_data)
     elif text == 'testaaa': # スクレピング検証用
-        result = sc.infected_people_scraping()
-        result_text = result[0] + "\n" + str(result[1]) + "\n" + str(result[2]) + "\n" + str(result[3])
+        print(sc.infected_people_scraping())
+        result_text = "スクレピング成功！！"
         make_quick_reply(event.reply_token, result_text)
     else:
         reply_text = "入力する言葉が違うよ！\n\n最新情報は\"最新\"\n一週間の情報は\"一週間\"\n\nと入力してね！\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
