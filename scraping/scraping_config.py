977ca838d8d15d4873426c635f27ed7134072d64
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-06-20 12:53:27 +0900

    import文修正とファイル分割

--- scraping/scraping_config.py
+++ scraping/scraping_config.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 # WebDriverの設定
 def setting_web_driver() -> webdriver:
