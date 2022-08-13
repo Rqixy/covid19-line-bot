8c44995844826337e1ca394a0ea138091d1ffcd7
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-13 18:05:58 +0900

    変数名を適切な値に変更とコメント内容を変更

--- scraping_test/scraping.py
+++ scraping_test/scraping.py
@@ -4,27 +4,29 @@ import time
 
 # スクレピング実行部分
 def scraping(driver: webdriver, iframe_xpath: str, scraping_xpath: str) -> str:
-    result_text = ''
+    scraped_text = ''
 
-    # 非同期でWebページが表示されるため、取得できないことがあるため、
-    # Webページを更新し再度取得できるか試す。
+    # 取得したい要素が非同期で表示されるため、取得できないことがある。
+    # そのため、取得できなかった時はページを更新し再度取得できるか試す。(ただし、無限ループを避けるため、10回までの制限をかけておく)
     for i in range(10):
-        time.sleep(2)   # 
+        time.sleep(2)   # 明示的にページの読み込みを待機する
+
         # iframeに入る
-        iframe = driver.find_element(By.XPATH, iframe_xpath)
-        driver.switch_to.frame(iframe)
-        time.sleep(2)
-        result = driver.find_element(By.XPATH, scraping_xpath)
-        result_text = result.text
+        iframe_element = driver.find_element(By.XPATH, iframe_xpath)
+        driver.switch_to.frame(iframe_element)
+        time.sleep(2)   # 明示的にページの読み込みを待機する
+
+        element = driver.find_element(By.XPATH, scraping_xpath)
+        scraped_text = element.text
 
         # iframeから元のフレームに戻る
         driver.switch_to.default_content()
 
         # 取得できたらループを抜ける
-        if result_text != '':
+        if scraped_text != '':
             break
 
         # Webページを更新する
         driver.refresh()
 
-    return result_text
+    return scraped_text
