919a2a69889dcf7dbbc54ac1f60864561503ace7
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-14 15:28:46 +0900

    一部、機能ごとにファイルを分けた

--- db_test/infection/insert.py
+++ db_test/infection/insert.py
@@ -4,41 +4,37 @@ import psycopg2
 import db_test.config as config
 import print_day as PD
 import db_test.infection.unit.delete_old_data as DOD
-sys.path.append(os.path.join(os.path.dirname(__file__), '../scraping_test'))
-import scraping_test.infected_people as scraping
-
-db_url = config.DATABASE_URL
+import db_test.infection.check.new_infected_data as NID
+# sys.path.append(os.path.join(os.path.dirname(__file__), '../scraping_test'))
+import scraping_test.infected_people as SC
 
 # 感染者データの追加
-def insert_infected_data():
-    # スクレイピングを行い、配列で取得する
-    infected_info = scraping.infected_people_scraping()
+def insert_infected_data() -> (list | str):
+    # スクレイピングを行い、配列で感染情報を取得する
+    infected_info = SC.infected_people_scraping()
 
-    # スクレイピングで情報が取得できていなかったら情報を取得できなかったことを送信する
+    # スクレイピングで情報が正しく取得できていなかったら情報を取得できなかったことを送信する
     if infected_info == None:
         text = "情報が正しく取得されませんでした\n午後6時にもう一度送信されます！"
         return text
 
+    # 情報が取得されていたら、変数に格納する
+    infected_day = infected_info[0]
+    new_people = infected_info[1]
+    severe_people = infected_info[2]
+    deaths = infected_info[3]
+
     # データベースに接続する
-    with psycopg2.connect(db_url) as conn:
+    with psycopg2.connect(config.DATABASE_URL) as conn:
         with conn.cursor() as curs:
             # もし新しいデータが入ってこなかったら新しいデータが無いことを送信する
-            sql = "SELECT * FROM test_table ORDER BY id DESC LIMIT 1;"
-            curs.execute(sql)
-            records = curs.fetchall()
-            result = ""
-            for row in records:
-                result = str(row[4])
-
-            # スクレイピングで新しく取得した日付と、最後のレコードにある日付を比較して
-            # 同じなら更新されていないことを伝える
-            if result == infected_info[0]:
+            if not NID.new_infected_data(curs, infected_info):
                 text = "新しい感染者情報が更新されていません！\n午後6時にもう一度送信されます！"
                 return text
 
             # スクレイピングで取ってきた配列のデータを格納する
             sql = "INSERT INTO test_table (new_people, severe_people, deaths, infected_day) VALUES (%s, %s, %s, %s)"
-            curs.execute(sql, (infected_info[1], infected_info[2], infected_info[3], infected_info[0]))
+            curs.execute(sql, (new_people, severe_people, deaths, infected_day))
 
             # レコードが7個より大きくなったら一番古いレコードを削除する
             DOD.delete_old_data(curs)
@@ -47,3 +43,5 @@ def insert_infected_data():
             new_data = PD.print_infected_day(0)
 
             return new_data
+
+print(insert_infected_data())
