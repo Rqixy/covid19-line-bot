25f9fe3ee7a821938e0113b217a7fa7ef06c4c53
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-14 15:26:25 +0900

    変数名を適切な名前に変更

--- db_test/infection/print_day.py
+++ db_test/infection/print_day.py
@@ -2,11 +2,11 @@ import db_test.config as config
 import psycopg2
 
 # 引数で与えられたレコードを返す
-def print_infected_day(day: int):
+def print_infected_day(day: int) -> (list | str):
     # もし送られてきた数字が１週間の範囲外ならエラー
     if day < 0 or day >= 7:
-        text = "入力された値が範囲外だよ！"
-        return text
+        error_text = "入力された値が範囲外だよ！"
+        return error_text
 
     # データベースに接続する
     with psycopg2.connect(config.DATABASE_URL) as conn:
@@ -14,7 +14,7 @@ def print_infected_day(day: int):
             sql = "SELECT * FROM test_table ORDER BY id DESC LIMIT 1 OFFSET %s;"
             curs.execute(sql, (str(day),))
 
-            new_data = []
-            for infected_data in curs.fetchall():
-                new_data = [infected_data[4], "    新規感染者数：" + str(infected_data[1]) + "人", "    重症者数(累計)：" + str(infected_data[2]) + "人", "    死亡者数(累計)：" + str(infected_data[3]) + "人"]
-            return new_data
+            infected_info_array = []
+            for infected_info in curs.fetchall():
+                infected_info_array = [infected_info[4], "    新規感染者数：" + str(infected_info[1]) + "人", "    重症者数(累計)：" + str(infected_info[2]) + "人", "    死亡者数(累計)：" + str(infected_info[3]) + "人"]
+            return infected_info_array
--- db_test/infection/print_week.py
+++ db_test/infection/print_week.py
@@ -2,14 +2,14 @@ import db_test.config as config
 import psycopg2
 
 # 1週間分の感染情報を取得して返す
-def print_week_infected_data():
+def print_infected_week() -> list:
     # データベースに接続する
     with psycopg2.connect(config.DATABASE_URL) as conn:
         with conn.cursor() as curs:
             sql = "SELECT * FROM test_table ORDER BY id;"
             curs.execute(sql)
-            week_data = []
-            for row in curs.fetchall():
-                week_data.append(row[4] + "\n" + "    新規感染者数：" + str(row[1]) + "人" + "\n" + "    重症者数(累計)：" + str(row[2]) + "人" + "\n" + "    死亡者数(累計)：" + str(row[3]) + "人\n\n")
+            week_infected_info_array = []
+            for infected_info in curs.fetchall():
+                week_infected_info_array.append(infected_info[4] + "\n" + "    新規感染者数：" + str(infected_info[1]) + "人" + "\n" + "    重症者数(累計)：" + str(infected_info[2]) + "人" + "\n" + "    死亡者数(累計)：" + str(infected_info[3]) + "人\n\n")
             
-            return week_data
+            return week_infected_info_array
