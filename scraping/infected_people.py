3cade1966fb7319d2c1509a64bf69999f0b24ced
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-14 22:07:04 +0900

    xpathの連想配列を関数外に記入

--- scraping/infected_people.py
+++ scraping/infected_people.py
@@ -2,36 +2,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import scraping.config as config
 import scraping.people as PP
-import scraping.infected_day as IDAY
+import scraping.infected_day as ID
 import scraping.check.get_info as GI
+
+# 取得したい情報の要素までのxpathを連想配列に格納する
+# 取得したい情報がiframeを使用して表示されているのでirame内に入るためのxpathも用意しておく
+
+# 感染日のxpath
+infected_day_xpaths = {
+    'iframe': '/html/body/div[1]/main/div[2]/div/div/div[3]/div/iframe',
+    'xpath': '/html/body/main/div/div/div/div/div/h3/span'
+}
+# 新規感染者数のxpath
+new_infected_xpaths = {
+    'iframe': '/html/body/div[1]/main/div[2]/div/div/div[4]/div[1]/iframe',
+    'xpath': '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
+}
+# 重症者数のxpath
+severe_xpaths = {
+    'iframe': '/html/body/div[1]/main/div[2]/div/div/div[4]/div[4]/iframe',
+    'xpath': '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
+}
+# 死亡者数のxpath
+deaths_xpaths = {
+    'iframe': '/html/body/div[1]/main/div[2]/div/div/div[4]/div[3]/iframe',
+    'xpath': '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
+}
 
 # 感染者情報をスクレイピングする
 def infected_people_scraping() -> (list | None):
     try:
-        # 取得したい情報の要素までのxpathを連想配列に格納する
-        # 取得したい情報がiframeを使用して表示されているのでirame内に入るためのxpathも用意しておく
-
-        # 感染日のxpath
-        infected_day_xpaths = {
-            'iframe': '/html/body/div[1]/main/div[2]/div/div/div[3]/div/iframe',
-            'xpath': '/html/body/main/div/div/div/div/div/h3/span'
-        }
-        # 新規感染者数のxpath
-        new_infected_xpaths = {
-            'iframe': '/html/body/div[1]/main/div[2]/div/div/div[4]/div[1]/iframe',
-            'xpath': '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
-        }
-        # 重症者数のxpath
-        severe_xpaths = {
-            'iframe': '/html/body/div[1]/main/div[2]/div/div/div[4]/div[4]/iframe',
-            'xpath': '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
-        }
-        # 死亡者数のxpath
-        deaths_xpaths = {
-            'iframe': '/html/body/div[1]/main/div[2]/div/div/div[4]/div[3]/iframe',
-            'xpath': '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
-        }
-
         # WebDriverの設定
         driver = config.setting_web_driver()
         # 指定したURLに遷移
@@ -42,7 +42,7 @@ def infected_people_scraping() -> (list | None):
 
         infected_info = []
 
-        infected_day = IDAY.infected_day_scraping(driver, infected_day_xpaths)
+        infected_day = ID.infected_day_scraping(driver, infected_day_xpaths)
         infected_info.append(infected_day)
         
         new_infected_people = PP.people_scraping(driver, new_infected_xpaths)
