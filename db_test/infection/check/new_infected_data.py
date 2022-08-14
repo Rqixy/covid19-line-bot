8e35c2fef94fe789bc97444d96eea2afd4f879a5
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-14 17:18:17 +0900

    import文のasの省略文字を適切な文字に修正

--- db_test/infection/check/new_infected_data.py
+++ db_test/infection/check/new_infected_data.py
@@ -1,8 +1,8 @@
-import db_test.infection.unit.japanese_to_western_calendar as JTO
+import db_test.infection.unit.japanese_to_western_calendar as JtW
 
 # 新しく取得した日付と、最新のレコードにある日付を比較して
 # 最新の日付が取得できているかチェックする
-def new_infected_data(curs, new_infected_day: str) -> bool:
+def new_infected_day(curs, new_infected_day: str) -> bool:
     sql = "SELECT * FROM test_table ORDER BY id DESC LIMIT 1;"  # DB内での最新のデータ
     curs.execute(sql)
     latest_infected_info_in_db = curs.fetchall()
@@ -13,8 +13,8 @@ def new_infected_data(curs, new_infected_day: str) -> bool:
         latest_infected_day_in_db = str(infected_info[4])
 
     # 各日付をdatetime型に変換
-    converted_new_infected_day = JTO.japanese_calendar_converter(new_infected_day)
-    converted_latest_infected_day_in_db = JTO.japanese_calendar_converter(latest_infected_day_in_db)
+    converted_new_infected_day = JtW.japanese_calendar_converter(new_infected_day)
+    converted_latest_infected_day_in_db = JtW.japanese_calendar_converter(latest_infected_day_in_db)
 
     # DB内の最新の日付と送られてきた日付を比較し、
     # 送られてきた日付がDB内の日付より前の日付か、同じ日付だったらfalseを返す
--- scraping/people.py
+++ scraping/people.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 import scraping.scraping as SC
 import scraping.unit.remove_comma as RC
-import scraping.unit.str_to_int as STI
+import scraping.unit.str_to_int as StI
 
 # 感染、重症、死亡の人数を取得するスクレピング
 def people_scraping(driver: webdriver, xpaths: dict) -> (int | None):
     try:
         str_people = SC.scraping(driver, xpaths)
         removed_comma_people = RC.remove_comma(str_people)
-        people = STI.str_to_int(removed_comma_people)
+        people = StI.str_to_int(removed_comma_people)
         
         return people
     except Exception as e:
