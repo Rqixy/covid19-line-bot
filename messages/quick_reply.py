ba2a4c3d2bd718c841b8f93f564cdc88c2e32952
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-15 15:02:52 +0900

    クイックメッセージ検証

--- messages/quick_reply.py
+++ messages/quick_reply.py
@@ -20,20 +20,20 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 # 返信用のクイックリプライの処理
 def quick_reply_for_reply(text, token):
     try:
-        quick_reply_button = [
-            QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')),
-            QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')),
-            QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')),
-            QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')),
-            QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')),
-        ]
+        # quick_reply_button = [
+        #     QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')),
+        #     QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')),
+        #     QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')),
+        #     QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')),
+        #     QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')),
+        # ]
 
-        # quick_reply_button = []
-        # quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
-        # quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
-        # quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
-        # quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
-        # quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
+        quick_reply_button = []
+        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
+        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
+        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
+        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
+        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
         messages = TextSendMessage(text, QuickReply(quick_reply_button))
         return line_bot_api.reply_message(token, messages)
     except Exception as e:
