c0797b18ea60064bc76ac93e494b5e76734e46d8
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-16 02:59:27 +0900

    Lineに表示する感染情報メッセージを作成する関数作成

---
+++ messages/text_on_line.py
@@ -0,0 +1,5 @@
+# Lineで表示される感染情報のテキスト作成
+
+def infected_info_on_line(infected_info) -> str:
+    line_text = infected_info[0] + "\n" + infected_info[1] + "\n" + infected_info[2] + "\n" + infected_info[3] + "\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
+    return line_text
