f601a76145ca8030c0111613dc0fe918389780c2
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-16 02:56:18 +0900

    関数名変更

--- db/infection/check/new_infected_day.py
+++ db/infection/check/new_infected_day.py
@@ -2,11 +2,10 @@ from db.infection.unit.japanese_to_western_calendar import japanese_calendar_con
 
 # 新しく取得した日付と、最新のレコードにある日付を比較して
 # 最新の日付が取得できているかチェックする
-def check_new_infected_day(curs, new_infected_day: str) -> bool:
-    sql = "SELECT * FROM infected_people ORDER BY id DESC LIMIT 1;"  # DB内での最新のデータ
+def check_new_infected_info(curs, new_infected_day: str) -> bool:
+    sql = "SELECT * FROM infected_people ORDER BY id DESC LIMIT 1;"
     curs.execute(sql)
-    latest_infected_info_in_db = curs.fetchall()
-    latest_infected_day_in_db = ""
+    latest_infected_info_in_db = curs.fetchall()    # DB内での最新のデータを取得
 
     # DB内の最新の日付のみ取得
     for infected_info in latest_infected_info_in_db:
