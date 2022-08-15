d88f88620af8c68a66c8659547432dbb3de5d37d
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-15 15:46:12 +0900

    配列を関数外に記入

--- messages/quick_reply.py
+++ messages/quick_reply.py
@@ -8,15 +8,22 @@ import os
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
+items = []
+items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
+items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
+items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
+items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
+items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
+
 # 送信用のクイックメッセージの処理
 def quick_reply_for_send(user_id, text):
     try:
-        items = []
-        items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
-        items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
-        items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
-        items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
-        items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
+        # items = []
+        # items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
+        # items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
+        # items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
+        # items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
+        # items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
         messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
 
         return line_bot_api.push_message(user_id, messages=messages)
@@ -26,12 +33,12 @@ def quick_reply_for_send(user_id, text):
 # 返信用のクイックリプライの処理
 def quick_reply_for_reply(token, text):
     try:
-        items = []
-        items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
-        items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
-        items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
-        items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
-        items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
+        # items = []
+        # items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
+        # items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
+        # items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
+        # items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
+        # items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
         messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
 
         return line_bot_api.reply_message(token, messages=messages)
