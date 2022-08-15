130f82a5c16d7ed33108832031f6163b8518dfff
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-15 15:39:28 +0900

    クイックメッセージ検証中

--- messages/quick_reply.py
+++ messages/quick_reply.py
@@ -18,7 +18,7 @@ quick_reply_button = [
 ]
 
 # 返信用のクイックリプライの処理
-def quick_reply_for_reply(token, text):
+def quick_reply_for_reply(reply_token, text):
     try:
         # quick_reply_button = [
         #     QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')),
@@ -28,15 +28,15 @@ def quick_reply_for_reply(token, text):
         #     QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')),
         # ]
 
-        # items = []
-        # items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
-        # items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
-        # items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
-        # items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
-        # items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
-        messages = TextSendMessage(text=text, quick_reply=QuickReply(items=quick_reply_button))
+        items = []
+        items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
+        items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
+        items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
+        items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
+        items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
+        messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
 
-        return line_bot_api.push_message(token, messages)
+        return line_bot_api.push_message(reply_token, messages)
     except Exception as e:
         print("メッセージエラー発生！ : " + str(type(e)) + " : " + e.args + " : " + e.message)
 
