7c4893b4dd2d3d5c7c1d55ae0a1dd31e1348d5ca
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-16 02:58:37 +0900

    変数名をoldestに変更

--- db/infection/unit/oldest_info_id.py
+++ db/infection/unit/oldest_info_id.py
@@ -1,9 +1,8 @@
 # 一番古いレコードを削除するためのid取得をして返す
-def oldest_info_id(curs):
+def oldest_info_id(curs) -> str:
     sql = "SELECT * FROM infected_people ORDER BY id LIMIT 1;"
     curs.execute(sql)
-    records = curs.fetchall()
-    first_data_id = ""
-    for row in records:
-        first_data_id = str(row[0])
-    return first_data_id
+    for infected_info in curs.fetchall():
+        oldest_id = str(infected_info[0])
+
+    return oldest_id
