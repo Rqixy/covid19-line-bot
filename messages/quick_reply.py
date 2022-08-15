3d5e986b42c2141af0cd1ed1af791c063c8a0d19
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-15 15:42:53 +0900

    元に戻した

--- messages/quick_reply.py
+++ messages/quick_reply.py
@@ -8,26 +8,9 @@ import os
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
-# クイックメッセージボタンの配列
-quick_reply_button = [
-    QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')),
-    QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')),
-    QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')),
-    QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')),
-    QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')),
-]
-
-# 返信用のクイックリプライの処理
-def quick_reply_for_reply(reply_token, text):
+# 送信用のクイックメッセージの処理
+def quick_reply_for_send(user_id, text):
     try:
-        # quick_reply_button = [
-        #     QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')),
-        #     QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')),
-        #     QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')),
-        #     QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')),
-        #     QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')),
-        # ]
-
         items = []
         items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
         items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
@@ -36,20 +19,21 @@ def quick_reply_for_reply(reply_token, text):
         items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
         messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
 
-        return line_bot_api.push_message(reply_token, messages)
+        return line_bot_api.push_message(user_id, messages=messages)
     except Exception as e:
         print("メッセージエラー発生！ : " + str(type(e)) + " : " + e.args + " : " + e.message)
 
-# 自動送信用のクイックメッセージの処理
-def quick_reply_for_send(user_id, text):
+# 返信用のクイックリプライの処理
+def quick_reply_for_reply(token, text):
     try:
-        quick_reply_button = []
-        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
-        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
-        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
-        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
-        quick_reply_button.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
-        messages = TextSendMessage(text, QuickReply(quick_reply_button))
-        return line_bot_api.push_message(user_id, messages)
+        items = []
+        items.append(QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')))
+        items.append(QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')))
+        items.append(QuickReplyButton(action=PostbackAction(label='一昨日', data='一昨日', text='一昨日')))
+        items.append(QuickReplyButton(action=PostbackAction(label='3日前', data='3日前', text='3日前')))
+        items.append(QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間')))
+        messages = TextSendMessage(text=text, quick_reply=QuickReply(items=items))
+
+        return line_bot_api.reply_message(token, messages=messages)
     except Exception as e:
-        print("メッセージエラー発生！ : " + str(type(e)) + " : " + e.args + " : " + e.message)
+        print("メッセージエラー発生！ : " + str(type(e)) + " : " + e.args + " : " + e.message) 
