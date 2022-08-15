57c4be28a7fb4e6dd16145b9ed71e1f2d33284af
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-15 14:45:26 +0900

    引数の受け取る順番変更

--- messages/infected_info_reply.py
+++ messages/infected_info_reply.py
@@ -4,9 +4,10 @@ from messages.quick_reply import quick_reply_for_reply
 # 1日分のコロナ感染者情報の送信の処理
 def infected_info_reply(event: any, day: int):
     infected_info = print_infected_day(day)
+
     # もし1週間の範囲外の数値が与えられたら範囲外のメッセージを送信する
     if type(infected_info) is str:
-        quick_reply_for_reply(event.reply_token, text=infected_info)
+        quick_reply_for_reply(infected_info, event.reply_token)
         return
     line_text_new_data = infected_info[0] + "\n" + infected_info[1] + "\n" + infected_info[2] + "\n" + infected_info[3] + "\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
-    quick_reply_for_reply(event.reply_token, text=line_text_new_data)
+    quick_reply_for_reply(line_text_new_data, event.reply_token)
