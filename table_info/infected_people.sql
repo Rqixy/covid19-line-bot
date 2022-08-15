f132385ee5f07f2e3bef67947a21f77a4d2a91c5
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-15 18:09:14 +0900

    table_info削除

--- table_info/infected_people.sql
+++
@@ -1,9 +0,0 @@
-infected_people (
-    id SERIAL NOT NULL,
-    new_people INTEGER NOT NULL,
-    severe_people INTEGER NOT NULL,
-    deaths INTEGER NOT NULL,
-    infected_day VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
-);
--- table_info/users_id.sql
+++
@@ -1,6 +0,0 @@
-users_id (
-    id SERIAL NOT NULL,
-    user_id VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
-);
