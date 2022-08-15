fc715d700679f1e56b505fd0cc32fce37741ed07
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-16 03:03:57 +0900

    import文の順番と複数行にわたるコメントアウト方法変更

--- scraping/infected_info.py
+++ scraping/infected_info.py
@@ -1,13 +1,15 @@
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from scraping.setting import setting_web_driver
-from scraping.people import people_scraping
+from selenium.webdriver.support.ui import WebDriverWait
+
+from scraping.check.get_info import check_get_info
 from scraping.infected_day import infected_day_scraping
-from scraping.check.get_info import get_info
+from scraping.people import people_scraping
+from scraping.setting import setting_web_driver
 
-# 取得したい情報の要素までのxpathを連想配列に格納する
-# 取得したい情報がiframeを使用して表示されているのでirame内に入るためのxpathも用意しておく
-
+"""
+取得したい情報の要素までのxpathを連想配列に格納する 
+取得したい情報がiframeを使用して表示されているのでirame内に入るためのxpathも用意しておく
+"""
 # 感染日のxpath
 infected_day_xpaths = {
     'iframe': '/html/body/div[1]/main/div[2]/div/div/div[3]/div/iframe',
@@ -29,13 +31,14 @@ deaths_xpaths = {
     'xpath': '/html/body/main/div/div/div[3]/div[1]/p[2]/span[1]'
 }
 
-# 感染者情報をスクレイピングする
+# 感染者情報をブラウザからスクレイピングして取得する
 def infected_info_scraping() -> (list | None):
     try:
         # WebDriverの設定
         driver = setting_web_driver()
         # 指定したURLに遷移
         driver.get('https://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html')
+
         # ページが読み込まれるまで待機
         wait = WebDriverWait(driver, 10)
         wait.until(EC.presence_of_all_elements_located)
@@ -58,7 +61,7 @@ def infected_info_scraping() -> (list | None):
         driver.quit()
         
         # 一つでも情報が取得出来ていなかったら、Noneを返す
-        if not get_info(infected_info):
+        if not check_get_info(infected_info):
             return None
 
         return infected_info
