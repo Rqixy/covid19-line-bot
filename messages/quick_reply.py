8b0f4be6c73b14ca32df287946bbe14727a9e74e
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-15 16:08:09 +0900

    コメント追加

--- messages/quick_reply.py
+++ messages/quick_reply.py
@@ -8,6 +8,7 @@ import os
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
+# クイックリプライボタン作成
 quick_reply_buttons = [
     QuickReplyButton(action=PostbackAction(label='最新情報', data='最新情報', text='最新情報')),
     QuickReplyButton(action=PostbackAction(label='昨日', data='昨日', text='昨日')),
@@ -16,14 +17,6 @@ quick_reply_buttons = [
     QuickReplyButton(action=PostbackAction(label='1週間', data='1週間', text='1週間'))
 ]
 
-# 送信用のクイックメッセージの処理
-def quick_reply_for_send(user_id, text):
-    try:
-        messages = TextSendMessage(text=text, quick_reply=QuickReply(items=quick_reply_buttons))
-        return line_bot_api.push_message(user_id, messages=messages)
-    except Exception as e:
-        print("メッセージエラー発生！ : " + str(type(e)) + " : " + e.args + " : " + e.message)
-
 # 返信用のクイックリプライの処理
 def quick_reply_for_reply(token, text):
     try:
@@ -31,3 +24,11 @@ def quick_reply_for_reply(token, text):
         return line_bot_api.reply_message(token, messages=messages)
     except Exception as e:
         print("メッセージエラー発生！ : " + str(type(e)) + " : " + e.args + " : " + e.message) 
+
+# 自動送信用のクイックリプライの処理
+def quick_reply_for_send(user_id, text):
+    try:
+        messages = TextSendMessage(text=text, quick_reply=QuickReply(items=quick_reply_buttons))
+        return line_bot_api.push_message(user_id, messages=messages)
+    except Exception as e:
+        print("メッセージエラー発生！ : " + str(type(e)) + " : " + e.args + " : " + e.message)
