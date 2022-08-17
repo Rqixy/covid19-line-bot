8a85b52923dd815335149dcf0be16c516a77b286
Rqixy <67447178+Rqixy@users.noreply.github.com>
2022-08-18 02:56:54 +0900

    コメント修正

--- messages/text_on_line.py
+++ messages/text_on_line.py
@@ -1,4 +1,4 @@
-# Lineで表示される感染情報のテキスト作成
+# Lineで表示する感染情報のテキスト作成
 
 def infected_info_on_line(infected_info) -> str:
     line_text = infected_info[0] + "\n" + infected_info[1] + "\n" + infected_info[2] + "\n" + infected_info[3] + "\n\n詳しい感染状況はこちらのサイトから確認してね！\nhttps://www.mhlw.go.jp/stf/covid-19/kokunainohasseijoukyou.html\n"
